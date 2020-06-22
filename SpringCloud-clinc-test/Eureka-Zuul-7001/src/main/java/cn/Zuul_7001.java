5f7dd1e837f0444b6056916675b0656834363a7e
syk123123 <5593129+syk123123@user.noreply.gitee.com>
2020-06-22 08:09:28 +0800

    更新

--- SpringCloud-clinc-test/Eureka-Zuul-7001/src/main/java/cn/Zuul_7001.java
+++ SpringCloud-clinc-test/Eureka-Zuul-7001/src/main/java/cn/Zuul_7001.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class Zuul_7001 {
     public static void main(String[] args) {
+        //Zuul
         SpringApplication.run(Zuul_7001.class,args);
     }
 
